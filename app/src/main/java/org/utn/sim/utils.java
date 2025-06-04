415727342b9a8c2f0affc4ea18b79e30389a6e61
Ignacio Zabalza <94262@sistemas.frc.utn.edu.ar>
2025-06-04 11:41:39 -0300

    funciones para generacion de variables aleatorias exp neg y uniforme (a,b) creadas en utils

---
+++ app/src/main/java/org/utn/sim/utils.java
@@ -0,0 +1,18 @@
+package org.utn.sim;
+
+public class utils {
+
+    // aca llevamos a cabo el calculo de la variable exponencial negativa, tomando
+    // como parametros la media y el random, que se pueden modificar por entrada del
+    // usuario
+    public static double exponencialNegativa(double media, double random) {
+        return -media * Math.log(1 - random);
+    }
+
+    // lo mismo que en el anterior pero con la variable uniforme a,b donde se pueden
+    // cargar ambos limites y el random
+    // en este caso la vamos a usar varias veces por lo tanto es mejor generalizarla
+    public static double uniforme(double LimiteInferior, double LimiteSuperior, double random) {
+        return LimiteInferior + (LimiteSuperior - LimiteInferior) * random;
+    }
+}
