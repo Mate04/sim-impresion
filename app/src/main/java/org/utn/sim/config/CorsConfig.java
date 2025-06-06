7e1c01e6997f3e1b5ca3a822b8815a31a1b4e79a
Lucia Bossio <91461@sistemas.frc.utn.edu.ar>
2025-06-05 21:13:22 -0300

    cors

---
+++ app/src/main/java/org/utn/sim/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.utn.sim.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Asegúrate de poner la URL correcta de tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
